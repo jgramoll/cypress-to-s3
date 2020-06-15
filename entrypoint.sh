d0f54db2ca48aef729b1a25d403349564e02ede8
jgramoll <jgramoll@instructure.com>
2020-06-15 18:23:46 -0500

    trim leading / in s3_path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #! /usr/bin/env bash
 
+# trim leading / so we don't get bucket//file
+s3_path="${S3_PATH#/}"
+s3_bucket="${S3_BUCKET}"
+
 cypress run $@
 exit_code=$?
 
 if [ $exit_code -ne 0 ]; then
-  if [ -v ${S3_BUCKET+x} ]; then
-    echo S3_BUCKET not provided. Unable to upload to s3.
+  if [ -z "${s3_bucket}" ]; then
+    echo s3_bucket not provided. Unable to upload to s3.
   else
-    echo Tests failed, uploading to "s3://${S3_BUCKET}/${S3_PATH}"
-    aws s3 sync ./cypress/screenshots/ "s3://${S3_BUCKET}/${S3_PATH}/screenshots/"
-    aws s3 sync ./cypress/videos/ "s3://${S3_BUCKET}/${S3_PATH}/videos/"
+    echo Tests failed, uploading to "s3://${s3_bucket}/${s3_path}"
+    aws s3 sync ./cypress/screenshots/ "s3://${s3_bucket}/${s3_path}/screenshots/"
+    aws s3 sync ./cypress/videos/ "s3://${s3_bucket}/${s3_path}/videos/"
   fi
   exit $exit_code
 fi
